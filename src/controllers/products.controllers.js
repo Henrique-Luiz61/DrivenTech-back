6359d883c69474751aaf1975e31825b0b00fe9de
nilteras <niltonrnascimento@hotmail.com>
2023-07-13 10:19:45 -0300

    feat: added /Post and /Delete products

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,4 +1,7 @@
+import { ObjectId } from 'mongodb';
 import { db } from '../database/database.connection.js';
+import { v4 as uuid } from 'uuid';
+
 
 export async function getProducts(req, res){
 
@@ -7,5 +10,32 @@ export async function getProducts(req, res){
         res.send(products)
     }catch (err){
         res.status(500).send(err.message)
+    }
+}
+
+export async function postProducts(req, res){
+
+    const { image, title, description, price } = req.body
+
+    try {
+        const product = {image, title, description, price}
+        await db.collection('products').insertOne(product)
+        res.sendStatus(201)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+export async function deleteProducts(req, res){
+    
+    const { id } = req.body
+    console.log(id)
+
+    try {
+        const product = await db.collection('products').deleteOne({ _id: new ObjectId(id) })
+        if(product.deletedCount === 0) return res.sendStatus(404)
+        res.send(200)
+    } catch (err) {
+        return res.status(500).send(err.message)
     }
 }
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { validateAuth } from '../middlewares/validateAuth.js'
-import { getProducts } from "../controllers/products.controllers.js";
+import { deleteProducts, getProducts, postProducts } from "../controllers/products.controllers.js";
 
 const productsRouter = Router();
 
 productsRouter.use(validateAuth)
 productsRouter.get('/products', getProducts)
+productsRouter.post('/products', postProducts)
+productsRouter.delete('/products', deleteProducts)
 
 export default productsRouter;
