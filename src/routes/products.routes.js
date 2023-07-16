b7113bf7fe2c8417ed2c7ec3da18e644b4c2716d
Henrique <henriqueluiz266@gmail.com>
2023-07-15 22:03:45 -0300

    atualizações back

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { validateAuth } from '../middlewares/validateAuth.js'
-import { deleteProducts, getProducts, postProducts } from "../controllers/products.controllers.js";
+import { validateAuth } from "../middlewares/validateAuth.js";
+import {
+  deleteProducts,
+  getProducts,
+  postProducts,
+} from "../controllers/products.controllers.js";
 
 const productsRouter = Router();
 
-productsRouter.use(validateAuth)
-productsRouter.get('/products', getProducts)
-productsRouter.post('/products', postProducts)
-productsRouter.delete('/products', deleteProducts)
+productsRouter.get("/products", getProducts);
+productsRouter.use(validateAuth);
+productsRouter.post("/products", postProducts);
+productsRouter.delete("/products", deleteProducts);
 
 export default productsRouter;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,10 +3,10 @@ import { validateSchema } from "../middlewares/validateSchema.js";
 import { schemaSingIn, schemaSingUp } from "../schemas/user.schemas.js";
 import { logOut, signIn, signUp } from "../controllers/user.controllers.js";
 
-const userRouter = Router()
+const userRouter = Router();
 
-userRouter.post('/sign-up', validateSchema(schemaSingUp), signUp);
-userRouter.post('/sign-in', validateSchema(schemaSingIn), signIn);
-userRouter.post('/logout', logOut);
+userRouter.post("/sign-up", validateSchema(schemaSingUp), signUp);
+userRouter.post("/sign-in", validateSchema(schemaSingIn), signIn);
+userRouter.post("/logout", logOut);
 
 export default userRouter;
